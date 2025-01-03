f1cf10f27b640d036f17353c2cd1228f8139195a
Rashad DuPaty <radupaty@gmail.com>
2025-01-03 11:40:35 +0900

    adding mobile header function

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -30,3 +30,9 @@ window.addEventListener("scroll", (event) => {
         }
     })
 });
+
+// Mobile Nav Trigger
+function toggleMenu() {
+  var menu = document.querySelector('nav ul');
+  menu.style.display = menu.style.display === 'none' ? 'flex' : 'none';
+}
